2febd8be0097656d6e5374f1079ab18bcbe3e0eb
skillt3ch <skillt3ch@gmail.com>
2017-03-31 00:07:56 +1100

    added word interactor challenge

--- codingtrain/2.9.1-word-interactor/sketch.js
+++ codingtrain/2.9.1-word-interactor/sketch.js
@@ -1,9 +1,11 @@
 var txt;
+var output;
 
 function setup() {
     noCanvas();
 
     txt = select("#txt");
+    output = select("#output");
     var submit = select("#submit");
 
     submit.mousePressed(regexSearch);
@@ -11,13 +13,37 @@ function setup() {
 
 function regexSearch() {
     var s = txt.value();
-    var r = /(\d{3})-(\d{4})/g;
+    var r = /(\W+)/;
 
-    var newstring = s.replace(r, replacer);
-    createP(newstring);
+    var words = s.split(r);
+    //console.log(words);
+
+    for (var i = 0; i < words.length; i++) {
+        var span = createSpan(words[i]);
+        span.parent(output);
+        if (!/\W+/.test(words[i])) {
+            //span.style("background-color", "#F0F");
+            span.mouseOver(highlight);
+        }
+    }
 }
 
 function replacer(match, grp1, grp2) {
-    console.log(arguments);
+    //console.log(arguments);
     return match;
+}
+
+function highlight() {
+    var s = this.html();
+    var star = "";
+
+    for (var i = 0; i < s.length; i++) {
+        star += "*"
+    }
+
+    //console.log(star);
+    this.html(star);
+
+    this.style("background-color", color(random(255), random(255), random(255)));
+    this.style("font-size", 30/s.length + 12 + "px");
 }
