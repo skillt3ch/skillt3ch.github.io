f0928e526e00048578d9195af07aa210ee1b917c
skillt3ch <skillt3ch@gmail.com>
2018-02-22 17:48:06 +1100

    Added button functionality

--- freeCodeCamp/Simon Game/index.js
+++ freeCodeCamp/Simon Game/index.js
@@ -1,12 +1,35 @@
 /* eslint-env browser */
 
-$(document).ready(function(){
+$(document).ready(function() {
+
+	function gameStart() {
+		console.log("Game Start");
+	}
+
+	function toggleStrict() {
+		$("#mode-led").toggleClass("led-on");
+	}
+
 	
-	
-	
-	
-	
-	$(".sw-slot").click(function(){
+//	When clicking the power button
+	$(".sw-slot").click(function() {
+		// toggle switch
 		$("#pwr-sw").toggleClass("sw-on");
+//		If the power button is off
+		if($("#pwr-sw").hasClass("sw-on")==false) {
+//			Initialise values and turn off LED display
+			$(".count").text("--");
+			$(".count").addClass("led-off");
+			$("#mode-led").removeClass("led-on");
+			$("#mode").off("click");
+			$("#start").off("click");
+		} else {
+//			Power button is on
+			$("#mode").click(toggleStrict);
+			$("#start").click(gameStart);
+			$(".count").removeClass("led-off");
+		}
 	});
+
+
 });
