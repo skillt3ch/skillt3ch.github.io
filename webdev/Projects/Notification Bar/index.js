052892fe49d539aa43d649961c64f58bc0449d8d
skillt3ch <skillt3ch@gmail.com>
2018-06-25 11:05:12 +1000

    added "technical documentation page" project

--- webdev/Projects/Notification Bar/index.js
+++ webdev/Projects/Notification Bar/index.js
@@ -1,9 +1,13 @@
 function close_notification() {
 	var bar = document.getElementById("notification");
+	bar.style.background = "rgba(153, 255, 219, 0)";
+	bar.style.opacity = 0;
 	bar.style.visibility = "hidden";
 }
 
 function show_notification() {
 	var bar = document.getElementById("notification");
+	bar.style.background = "rgba(153, 255, 219, 1)";
+	bar.style.opacity = 1;
 	bar.style.visibility = "visible";
 }
